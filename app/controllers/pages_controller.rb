892e8adb450e288977dda3fa65152f8ace7d9252
Jess Tada <jesstada@gmail.com>
2017-04-05 19:07:00 +1000

    stripe

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def host_dashboard
+    #get approved and not approved bookings
+    #render template or that post
+    @rooms = current_user.rooms #rooms that belong to the current user
+  end
+
+  def guest_dashboard
+    @bookings = current_user.bookings
+    @approved_bookings = @bookings.approved
+    @pending_bookings = @bookings.pending
+
+  end
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404005823) do
+ActiveRecord::Schema.define(version: 20170404011819) do
 
   create_table "bookings", force: :cascade do |t|
     t.date     "start_date"
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 20170404005823) do
     t.integer  "guest_number"
     t.integer  "room_id"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.boolean  "approved",     default: false
     t.index ["room_id"], name: "index_bookings_on_room_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
